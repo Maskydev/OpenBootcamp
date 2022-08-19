0e085b321085edfca486628165832b949ed691ca
Òscar Mascarós <Maskydev@gmail.com>
2022-08-19 16:14:01 +0200

    Ejercicio 3 - Control de flujo

---
+++ Curso_de_Python/Ejercicios_tema_4/Ejercicio 3/ejercicio_3.py
@@ -0,0 +1,7 @@
+# Escribe un programa que sea capaz de mostrar los números del 1 al 100 en orden inverso.
+
+numero_inicial = 100
+
+while (numero_inicial >= 1):
+    print(numero_inicial)
+    numero_inicial -= 1
